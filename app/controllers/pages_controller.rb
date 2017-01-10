0a6bee3966bb2ae5e6a2c483ed4816faf0731cde
Kristopher Corso <Kristopher@disabledperson.com>
2017-01-10 10:27:59 -0800

    Adding USDiversityjobs.org

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,8 @@
 class PagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def about
-  end
+  def about; end
 
-  def jobs
-    #require "HTTParty"
-    #require "nokogiri"
-    #require 'JSON'
+  def jobs; end
 
-    #page = HTTParty.get("https://www.disabledperson.com")
-
-    #parse_page = Nokogiri::HTML(page)
-
-    #@dp_jobs = []
-
-    #parse_page.css('.content').css('a').map do |a|
-      #post_name = a
-      #@dp_jobs << post_name
-    #end
-  end
-
-  def fix_url(str)
-    #need to take the string from Nokogiri and take away the <a href>
-    #need to grab the jobs/categories/CategoryName and assign it to a variable
-    #need to prepend https://www.disabledperson.com to the beginning of variable
-    #need to make the new string a link
-  end
 end
